fe73b41a113fcc928111cd1fda6e7c583c615794
truhvan <truhvanvan@yandex.com>
2022-05-12 14:03:31 +0500

    Add random functions Add getRandomInt(max) function & computerTurn() functions

--- game.js
+++ game.js
@@ -1,1 +1,20 @@
 console.log("a");
+
+// Get random int from 0 to max */
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+// Randomly return "Rock", "Paper" or "Scissors"
+computerTurn = () => {
+    switch (getRandomInt(3)) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+        default:
+            return "Something Went Wrong";
+    }
+}
